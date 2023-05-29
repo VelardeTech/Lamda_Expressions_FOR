14f5d3cd35b5ea743485b07eae5990d889c2b9b6
velardetech <velarde221upc@gmail.com>
2023-05-28 23:18:35 -0500

    Comentario

--- main.cpp
+++ main.cpp
@@ -9,9 +9,11 @@ using namespace System;
 int main() {
 	vector <int> num{ 1, 34, 20, 45, 66 };
 	list <char> letra{ 'a', 'h', 'k', 'j', 't' };
-	cout << "\n\tDisplay element in a lis using lamda" << endl; cout << "\t";
+	cout << "\n\tDisplay element in a vector using lamda" << endl; cout << "\t";
 	for_each(num.cbegin(), num.cend(), [](const int& element) {cout << element << "  "; }); cout << endl << endl;
-	cout << "\tDisplay element in a lis using lamda" << endl; cout << "\t";
+
+	//cbegin - cend - element
+	cout << "\tDisplay element in a list using lamda" << endl; cout << "\t";
 	for_each(letra.cbegin(), letra.cend(), [](auto& element) {cout << element << "  "; });
 	_getch(); return 0;
 }
